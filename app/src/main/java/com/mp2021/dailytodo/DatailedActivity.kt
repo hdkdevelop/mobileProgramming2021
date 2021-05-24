3d46dc797ea9019ee504710e4ed82f51c46aae0b
강현수 <2468abcdee@naver.com>
2021-05-25 00:16:16 +0900

    엑티비티 둘 추가. 메뉴바 추가. 일단 디테일에이어놓음. Add든 뭐든 추가해주면 해당에 연결할 예정

---
+++ app/src/main/java/com/mp2021/dailytodo/DatailedActivity.kt
@@ -0,0 +1,11 @@
+package com.mp2021.dailytodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datailed)
+    }
+}
--- app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.mp2021.dailytodo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+   // lateinit var intent:Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add -> {
+                var intent = Intent(this, DatailedActivity::class.java)
+                startActivity(intent)
+            }
+            //TODO 이름 몰라서 이렇게해놓음..
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
