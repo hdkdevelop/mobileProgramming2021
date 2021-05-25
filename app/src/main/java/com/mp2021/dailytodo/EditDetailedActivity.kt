a30692341c38dd50fbd458780552dfce82f85593
강현수 <2468abcdee@naver.com>
2021-05-25 16:08:37 +0900

    detail부분 레이아웃 완성. 나중에 사용할 별도 준비완료.

--- app/src/main/java/com/mp2021/dailytodo/EditDetailedActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/EditDetailedActivity.kt
@@ -2,12 +2,10 @@ package com.mp2021.dailytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 
-class DatailedActivity : AppCompatActivity() {
+class EditDetailedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_datailed)
+        setContentView(R.layout.activity_edit_detailed)
     }
 }
