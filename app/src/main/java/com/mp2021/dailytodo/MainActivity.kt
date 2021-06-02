89f2bd7bebe82836a69937882b9be99de7a93fe1
solar <lunar1514@naver.com>
2021-06-02 23:14:55 +0900

    add: git test in comment

--- app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // git test
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
