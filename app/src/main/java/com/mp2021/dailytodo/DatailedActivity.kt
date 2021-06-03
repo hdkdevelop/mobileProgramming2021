ea5252637863ac9127d260d4c4c8698ff3955538
강현수 <2468abcdee@naver.com>
2021-06-04 03:28:19 +0900

    init 복원.TODO-DB 변수들만 나오면 실행가능 db필요변수 추가 확인되서 추가. detailedactivity(datailed라고 잘못지었네요..) 보강

--- app/src/main/java/com/mp2021/dailytodo/DatailedActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/DatailedActivity.kt
@@ -5,9 +5,28 @@ import android.os.Bundle
 import android.view.Window
 
 class DatailedActivity : AppCompatActivity() {
+    lateinit var name: String
+    lateinit var date: String
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datailed)
+        if (intent.hasExtra("name")&&intent.hasExtra("date")){
+            name= intent.getStringExtra("name").toString()
+            date=intent.getStringExtra("date").toString()
+        }else{
+            name="error"
+            date="error"
+        }
+        init()
+    }
+    private fun init(){
+        //값 db에서 받아와야함.
+        //일단 판별부분은 이름과 작성일자로 판별예정.
+        //정보 전송과 수신은 put extra와 get extra사용.
+    }
+    override fun onResume() {
+        super.onResume()
+        //다시 변경된값 로드해야함.
     }
 }
--- app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-   // lateinit var intent:Intent
     lateinit var adapter: MyHabitAdapter
+    var data = ArrayList<MyHabit>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
        // git test
+        init()
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -37,5 +39,30 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
     //Todo 어댑터 구현., init구현.
+    override fun onResume() {
+        super.onResume()
+        //다시 로드될때, 정보들 초기화.
+        init()
+    }
+
+    private fun init(){
+        initData()
+        adapter= MyHabitAdapter(data)
+        adapter.itemClickListener = object : MyHabitAdapter.OnItemClickListener{
+            override fun OnItemClick(holder: MyHabitAdapter.ViewHolder, view: View, data: MyHabit, position: Int) {
+                var i = Intent(this@MainActivity, AddhabitActivity::class.java)
+                i.putExtra(data.habit_name,"name")
+                i.putExtra(data.start_date,"date")
+                startActivity(i)
+            }
+        }
+        //db에서 정보들 받아와야함.
+        //4가지 정보를 initData에서 받은뒤에, adapter초기화해주고, 3가지만 받는 규칙 제정해야함.- 그냥 EditDetailed에서 3개이상이면 작동하지 않도록 제한걸면될듯!
+        //즐겨찾기는 시작날짜와 이름으로 생각중.
+    }
+    private fun initData(){
+        //db에서 정보 받아와야함
+    }
 }
