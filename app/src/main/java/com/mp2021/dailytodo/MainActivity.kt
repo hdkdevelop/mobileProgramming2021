4f39b3f88ad54db087f5148f28357ed99fe6244d
강현수 <2468abcdee@naver.com>
2021-06-04 02:38:50 +0900

    init 구현했었는데 update과정에서 날아가서 다시작성예정

--- app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
    // lateinit var intent:Intent
+    lateinit var adapter: MyHabitAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
