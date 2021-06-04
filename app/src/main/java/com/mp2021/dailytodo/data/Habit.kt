0c88188b0dbc0731e6c37c2eec662431d8e57bf1
Dahyunne <ryudh98@gmail.com>
2021-06-05 01:37:03 +0900

    Habit 클래스 오타 수정

--- app/src/main/java/com/mp2021/dailytodo/data/Habit.kt
+++ app/src/main/java/com/mp2021/dailytodo/data/Habit.kt
@@ -3,4 +3,4 @@ package com.mp2021.dailytodo.data;
 import java.io.Serializable
 import java.util.*
 
-data class Habit(var id: Long?, var title: String, var categoryId: Long?, var startDate: Date, var detail:String, var favored:Boolean,var catagotyname:String): Serializable {}
+data class Habit(var id: Long?, var title: String, var categoryId: Long?, var startDate: Date, var detail:String, var favored:Boolean,var catagoryname:String): Serializable {}
