49c27633095ce5afdf1d918dfb0af4d37377e72f
Dahyunne <ryudh98@gmail.com>
2021-06-05 01:37:52 +0900

    추가화면 기능 조금 구현
    id는 어떻게 하면 될까 고민중

--- app/src/main/java/com/mp2021/dailytodo/AddcateActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/AddcateActivity.kt
@@ -3,6 +3,8 @@ package com.mp2021.dailytodo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import com.mp2021.dailytodo.databinding.ActivityAddcateBinding
 
 class AddcateActivity : AppCompatActivity() {
@@ -14,6 +16,11 @@ class AddcateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddcateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var actionBar : ActionBar?
+        actionBar = supportActionBar
+        actionBar?.hide()
+
         init()
     }
 
@@ -26,12 +33,33 @@ class AddcateActivity : AppCompatActivity() {
 
             btadd.setOnClickListener {
                 //모든 내용 null이 아닐때만 카테고리 추가하고 습관목록화면으로 가기
+                val catename = catename.text.toString()
+                val catedetail = catedetail.text.toString()
+
+
+                if(catename == "" || catedetail == "") {
+                    Toast.makeText(this@AddcateActivity, "모든 항목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }else{
+                    //카테고리추가
+                }
+                clearEditText()
+                //이전화면으로
             }
             btcancel.setOnClickListener {
                 //변경사항 없이 뒤로가기
+                clearEditText()
+                finish()
             }
 
         }
 
     }
+
+    fun clearEditText(){
+        binding.apply {
+            catename.text.clear()
+            catedetail.text.clear()
+        }
+    }
+
 }
--- app/src/main/java/com/mp2021/dailytodo/AddhabitActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/AddhabitActivity.kt
@@ -1,23 +1,74 @@
 package com.mp2021.dailytodo
 
+import android.R
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.mp2021.dailytodo.data.Habit
 import com.mp2021.dailytodo.databinding.ActivityAddhabitBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddhabitActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAddhabitBinding
+    lateinit var DB: Database
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         binding = ActivityAddhabitBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var actionBar : ActionBar?
+        actionBar = supportActionBar
+        actionBar?.hide()
+
         init()
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+        val adapter = ArrayAdapter<String>(this, R.layout.simple_spinner_dropdown_item,ArrayList<String>())
+        //일단 추가
+        adapter.add("카테고리1")
+        adapter.add("카테고리2")
+        adapter.add("카테고리3")
+        binding.apply {
+            habitcatespinner.adapter = adapter
+            habitcatespinner.setSelection(0)
+            habitcatespinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    when (position) {
+                        0 -> {
+                        }
+                        1 -> {
+                        }
+                        2 -> {
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun init() {
+
+        DB = Database(this)
+        val adapter = ArrayAdapter<String>(this, R.layout.simple_spinner_dropdown_item,ArrayList<String>())
+        //일단 추가
+        adapter.add("카테고리1")
+        adapter.add("카테고리2")
+        adapter.add("카테고리3")
 
         binding.apply {
 
@@ -26,13 +77,42 @@ class AddhabitActivity : AppCompatActivity() {
 
             btadd.setOnClickListener {
                 //모든 내용 null이 아닐때만 습관 추가하고 습관목록화면으로 가기
+                val habitname = habitname.text.toString()
+                val habitdetail = habitdetail.text.toString()
+                val habitcategory = habitcatespinner.selectedItem.toString()
+
+                val currentDateTime = Calendar.getInstance().time
+
+                if(habitname == "" || habitdetail == "" || habitcategory == ""){
+                    Toast.makeText(this@AddhabitActivity, "모든 항목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }else{
+                    val habitset = Habit(1, habitname, 1, currentDateTime, habitdetail, false, habitcategory)
+                    val result = DB.insertHabit(habitset)
+                    if(result){
+                        Toast.makeText(this@AddhabitActivity, "습관 추가 성공", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@AddhabitActivity, "습관 추가 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                clearEditText()
+                //이전화면으로
             }
+
             btcancel.setOnClickListener {
                 //변경사항 없이 뒤로가기
-
+                clearEditText()
+                finish()
             }
 
 
         }
     }
+
+    fun clearEditText(){
+        binding.apply {
+            habitname.text.clear()
+            habitdetail.text.clear()
+        }
+    }
+
 }
