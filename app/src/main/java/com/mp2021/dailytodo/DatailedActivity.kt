47a84c48380fcb724096eea7ce139032d4650df4
강현수 <2468abcdee@naver.com>
2021-06-05 11:28:21 +0900

    아직도 자료 가져오는법 찾는중 detailed.kt일부 구현. editdetailed.xml수정

--- app/src/main/java/com/mp2021/dailytodo/DatailedActivity.kt
+++ app/src/main/java/com/mp2021/dailytodo/DatailedActivity.kt
@@ -1,11 +1,20 @@
 package com.mp2021.dailytodo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 
 class DatailedActivity : AppCompatActivity() {
     var id=-1
+    lateinit var nametext:TextView
+    lateinit var detailtext:TextView
+    lateinit var categorytext:TextView
+    lateinit var streaktext:TextView
+    lateinit var totaltext:TextView
+    lateinit var starttext:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
@@ -18,12 +27,48 @@ class DatailedActivity : AppCompatActivity() {
         init()
     }
     private fun init(){
-        //값 db에서 받아와야함.
-        //일단 판별부분은 이름과 작성일자로 판별예정.
-        //정보 전송과 수신은 put extra와 get extra사용.
+        var backbtn=findViewById<Button>(R.id.button1)
+        var favorbtn=findViewById<Button>(R.id.button2)
+        var editbtn=findViewById<Button>(R.id.button3)
+        nametext=findViewById(R.id.textView2)
+        detailtext=findViewById(R.id.textView4)
+        categorytext=findViewById(R.id.textView6)
+        streaktext=findViewById(R.id.textView8)
+        totaltext=findViewById(R.id.textView10)
+        starttext=findViewById(R.id.textView12)
+        inittext()
+        editbtn.setOnClickListener{
+            var intent = Intent(this, EditDetailedActivity::class.java)
+            intent.putExtra("name",nametext.text)
+            intent.putExtra("detail",detailtext.text)
+            intent.putExtra("category",categorytext.text)
+            intent.putExtra("streak",streaktext.text)
+            intent.putExtra("total",totaltext.text)
+            intent.putExtra("start",starttext.text)
+            intent.putExtra("id",id)
+            startActivity(intent)
+        }
+        backbtn.setOnClickListener{
+            onBackPressed()
+        }
+        favorbtn.setOnClickListener{
+            //Todo-즐겨찾기 난감하네
+        }
     }
     override fun onResume() {
         super.onResume()
-        //다시 변경된값 로드해야함.
+        inittext()
+    }
+    private fun inittext{
+        if(id==-1){
+            nametext.text="error"
+            detailtext.text="error"
+            categorytext.text="error"
+            streaktext.text="error"
+            totaltext.text="error"
+            starttext.text="error"
+        }else{
+            //TODO-db에서 정보가져와야되는데 모르겠음.
+        }
     }
 }
